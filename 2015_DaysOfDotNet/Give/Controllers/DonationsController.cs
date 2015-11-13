ef3afbc1985ff33808d558d7f9230fd98d8b9cf2
Darren Hale <darrenhale@darrenhale.clearent.lan>
2015-11-13 07:14:45 -0600

    Presentation and samples for St. Louis Days of .NET 2015.

---
+++ 2015_DaysOfDotNet/Give/Controllers/DonationsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyNamespace
+{
+    [Route("/donations")]
+    public class DonationsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2015_DaysOfDotNet/Give/Startup.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Give
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+          services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+          app.UseMvc();
+        }
+    }
+}
